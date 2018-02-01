3e1d24a33f6094a5d88c6d2cc770fec285630501
Pauli Rikula <pauli.rikula@gmail.com>
2018-02-01 22:05:49 +0200

    documentation

--- setup.py
+++ setup.py
@@ -13,6 +13,11 @@ setup(
     description='python-category-equations',
     long_description=category_equations.from_operator.__doc__,
     license="MIT",
-    py_modules=['category_equations'],
-    author="Pauli Rikula")
+    author="Pauli Rikula",
+    url='https://github.com/kummahiih/python-category-equations',
+    packages=['category_equations'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6']
+)
     
