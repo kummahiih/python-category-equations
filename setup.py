24763e2240594f0c25860f0d7986197d0a674a97
Pauli Rikula <pauli.rikula@gmail.com>
2026-01-15 12:14:23 +0200

    0.7.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'wt') as readme_file:
 
 setup(
     name='python-category-equations',
-    version='0.7.3',
+    version='0.7.4',
     description='python-category-equations',
     long_description=README,
     license="MIT",
